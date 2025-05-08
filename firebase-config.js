ea5b4de29a64cc05a8039a95bf880ccb69e2469c
damiaig <damilolaaig@gmail.com>
2025-05-08 18:43:13 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,6 +2,8 @@
 import { getStorage } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-storage.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-firestore.js"; 
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
+
 // Replace with your own config
 const firebaseConfig = {
     apiKey: "AIzaSyCNICRxGwTzDOwnYlpw1_CfzwMBdQ7Hvkk",
@@ -17,3 +19,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const auth = getAuth(app);
+
